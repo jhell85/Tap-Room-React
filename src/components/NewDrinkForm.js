8db27dfb0b204a277633fb4f8cafdbd2ab69f36d
jhell85 <jhell85@gmail.com>
2020-07-03 14:33:44 -0700

    add methods for adding drink

--- src/components/NewDrinkForm.js
+++ src/components/NewDrinkForm.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-function NewDrinkForm() {
+function NewDrinkForm(props) {
+  function handleNewDrinkFormSubmission(event){
+    event.preventDefault();
+    props.onNewDrinkCreation({name: event.target.name.value, brand: event.target.brand.value, alcoholContent: event.target.alcoholContent.value, price: event.target.price.value, drinksLeft: event.target.drinksLeft.value, id: v4()});
+  }
 return(
   <React.Fragment>
     <h3>NewDrinkForm</h3>
